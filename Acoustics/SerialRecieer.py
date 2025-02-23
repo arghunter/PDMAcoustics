a31c687e4ae1cdf5f17a970c5cc98bb8aac23215
Armaan G <acgo8888@gmail.com>
2025-02-22 16:35:26 -0800

    usb serial working

--- Acoustics/SerialRecieer.py
+++ Acoustics/SerialRecieer.py
@@ -1,9 +1,22 @@
 import serial
-
-ser = serial.Serial('COM5', 115200, timeout=1)
-
+import numpy as np
+import time
+ser = serial.Serial('COM5', 921600, timeout=1)
+past=np.zeros(4,dtype=int)
+pt=0
 while True:
     byte = ser.read(1)
-    if byte == b'\xAA':  # Detect start marker
-        data = ser.read(256)  # Read the full array
-        print("Received:", data)
+    past=np.roll(past,1)
+    past[0]=(byte[0])
+    print(str(past[0])+" "+str(pt))
+    pt+=1
+    if np.all(past==88):
+        pt=0
+        
+        
+          # Detect start marker
+    #     data = ser.read(256)  # Read the full array
+    #     bytearray()
+    #     print(len(data))
+    #     byte_array = np.frombuffer(data, dtype=np.uint8)  # Convert to numpy array of integers
+    #     print("Received as integer array:", byte_array)
