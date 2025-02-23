a478679108a0a7a1704886031eb11a079b5f1116
Armaan G <acgo8888@gmail.com>
2025-02-22 16:54:55 -0800

    vis done

--- Acoustics/SerialRecieer.py
+++ Acoustics/SerialRecieer.py
@@ -10,12 +10,14 @@ while times<60:
     byte = ser.read(1)
     past=np.roll(past,1)
     past[0]=(byte[0])
-    print(str(past[0])+" "+str(pt))
+    # print(str(past[0])+" "+str(pt))
     pt+=1
-    pdata[int(pt/16)][int(pt %16)][times]+=np.abs(past[0])
+    if(pt<256):
+        pdata[int(pt/16)][int(pt %16)][times]+=np.abs(past[0])
     if np.all(past==88):
         pt=0
         times+=1
+        print(times)
 
 import Visual
 Visual.gen_anim(pdata,16,2.191,1000,1,120)
